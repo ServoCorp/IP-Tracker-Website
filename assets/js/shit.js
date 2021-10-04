748dd22881ce90b9255bca82ec52426f67b5da73
ServoCorp <80141504+ServoCorp@users.noreply.github.com>
2021-10-04 14:46:58 -0500

    added more stable layout

--- assets/js/shit.js
+++ assets/js/shit.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
+var visitorIP = getIP();
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -10,14 +11,12 @@ L.marker([51.5, -0.09]).addTo(map)
 
 
 // >.> sorry
-
  function search() {
     var letters = /^[\D\s]+$/; // Any non whitepsace character & Any non digit character Regex
     var input = document.getElementById('searchBar').value; // Search input
     var baseurl = "http://api.ipstack.com/"
     var apiKey = "?access_key=" + config.apiKey;
     
-
     if (input.match(letters)) // see if it matches regex
     {
         window.alert("Please only use IPV4. Example: 192.168.1.1");
@@ -34,11 +33,25 @@ L.marker([51.5, -0.09]).addTo(map)
         .then(response => response.json())
         .then(json => console.log(json))
 
-        console.log(json)
         console.log(input);
         console.log("hello");
 
     }
+}
 
+function getIP() {
+    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 
+    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
+    xmlhttp.send();
+
+    hostipInfo = xmlhttp.responseText.split("\n");
+
+    for (i=0; hostipInfo.length >= i; i++) {
+        ipAddress = hostipInfo[i].split(":");
+        if ( ipAddress[0] == "IP" ) return ipAddress[1];
+    }
+
+    return false;
 }
