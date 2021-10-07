2fef840ceede37476d09b0ee48c2bdb401dda37c
ServoCorp <80141504+ServoCorp@users.noreply.github.com>
2021-10-06 21:37:25 -0500

    fixed some JS code | fixed layout

--- assets/js/shit.js
+++ assets/js/shit.js
@@ -1,5 +1,6 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
-var visitorIP = getIP();
+var baseurl = "http://api.ipstack.com/"
+var apiKey = "?access_key=" + config.apiKey;
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -14,8 +15,7 @@ L.marker([51.5, -0.09]).addTo(map)
  function search() {
     var letters = /^[\D\s]+$/; // Any non whitepsace character & Any non digit character Regex
     var input = document.getElementById('searchBar').value; // Search input
-    var baseurl = "http://api.ipstack.com/"
-    var apiKey = "?access_key=" + config.apiKey;
+
     
     if (input.match(letters)) // see if it matches regex
     {
@@ -39,19 +39,75 @@ L.marker([51.5, -0.09]).addTo(map)
     }
 }
 
-function getIP() {
-    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+function showStats() {
+    showIP();
+    getLocation()
+    getTimezone()
+    getISP()
+    returnLatLong()
+}
 
-    xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
-    xmlhttp.send();
+function showIP() { //using another free third party api so it doesn't use all api calls when visitor loads page >.>
+    data = new XMLHttpRequest();
+    data.responseType = 'json';
+    data.open("GET", 'http://ip-api.com/json/');
+    data.onload = function() {
+    var mySpan = document.getElementsByClassName("ipAddr")[0];
+    var txt = document.createTextNode(data.response.query);
+    mySpan.appendChild(txt);  
+};
+data.send();
+}
 
-    hostipInfo = xmlhttp.responseText.split("\n");
 
-    for (i=0; hostipInfo.length >= i; i++) {
-        ipAddress = hostipInfo[i].split(":");
-        if ( ipAddress[0] == "IP" ) return ipAddress[1];
-    }
+function getLocation() {
+    var data = new XMLHttpRequest();
+    data.responseType = 'json';
+    data.open("GET", 'http://ip-api.com/json/');
+    data.onload = function() {
+    var mySpan = document.getElementsByClassName("userLoc")[0];
+    var txt = document.createTextNode(data.response.city + ", " + data.response.regionName);
+    mySpan.appendChild(txt);
+    //console.log(data.response)  
+};
+data.send();
+}
 
-    return false;
+function getTimezone() {
+    var data = new XMLHttpRequest();
+    data.responseType = 'json';
+    data.open("GET", 'http://ip-api.com/json/');
+    data.onload = function() {
+    var mySpan = document.getElementsByClassName("userTime")[0];
+    var txt = document.createTextNode(data.response.timezone);
+    mySpan.appendChild(txt);
+};
+data.send();
+}
+
+function getISP() {
+    var data = new XMLHttpRequest();
+    data.responseType = 'json';
+    data.open("GET", 'http://ip-api.com/json/');
+    data.onload = function() {
+    var mySpan = document.getElementsByClassName("userIsp")[0];
+    var txt = document.createTextNode(data.response.isp);
+    mySpan.appendChild(txt);
+};
+data.send();
+}
+
+function returnLatLong() {
+    var data = new XMLHttpRequest();
+    data.responseType = 'json';
+    data.open("GET", 'http://ip-api.com/json/');
+    data.onload = function latLong() {
+        //console.log(data.response)
+
+        return data.response.lat; 
+};
+data.send();
+    //console.log(data.response.lat + data.response.long); 
+    console.log(data.response)
+    return latLong()
 }
